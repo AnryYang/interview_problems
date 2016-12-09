7a455bb6e3ade33b4f171ae3cdfe45d213e31aff
anryyang <anryyang@gmail.com>
2016-12-09 10:40:45 +0800

    init bloom filter

---
+++ Bloom_Filter.cc
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: Bloom_Filter.cc
+    > Author: anryyang
+    > Mail: anryyang@gmail.com 
+    > Created Time: Fri 09 Dec 2016 10:39:09 AM
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
